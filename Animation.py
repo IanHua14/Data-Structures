efea22318f9b01b06f5a54a698053ab80e70331e
chase <Chaseott123@gmail.com>
2022-03-13 11:45:53 -0700

    ghjtest

--- Animation.py
+++ Animation.py
@@ -1,41 +1,42 @@
-
-#funcy.py
 import time
 
 # terminal print commands
-ANx
+ANSI_CLEAR_SCREEN =  lambda: print('\n' * 150)
+ANSI_HOME_CURSOR = u"\u001B[0;0H\u001B[2"
+OCEAN_COLOR = u""
+SHIP_COLOR = u"\u001B[32m\u001B[2D"
+RESET_COLOR = u"\u001B[0m\u001B[2D"
 
-def ocean_print():
-    # print ocean
-    print(ANSI_CLEAR_SCREEN, ANSI_HOME_CURSOR)
-    print("\n\n\n\n")
-    print(OCEAN_COLOR + "  " * 35)
 
 
 # print ship with colors and leading spaces
-def ship_print(position):
-    print(ANSI_HOME_CURSOR)
+def Animation_print(position):
+    print('\n' * 10)
     print(RESET_COLOR)
     sp = " " * position
-    print(sp + "    |\   ")
-    print(sp + "    |/   ")
-    print(SHIP_COLOR, end="")
-    print(sp + "\__ |__/ ")
-    print(sp + " \____/  ")
+    print(sp + " |  ")
+    print(sp + " |  ")
+    print(sp + " +  \ ")
+    print(sp + " \ \.G_.*=.")
+    print(sp + "   `(H'/.\|")
+    print(sp + "    .>'  (_--.")
+    print(sp + "  =/d   ,^\ ")
+    print(sp + "~~ \)-'   '")
+    print(sp + "    / |")
+    print(sp + "   '  '   ")
     print(RESET_COLOR)
 
-
 # ship function, iterface into this file
-def ship():
-    # only need to print ocean once
-    ocean_print()
+def animation():
 
     # loop control variables
     start = 0  # start at zero
-    distance = 60  # how many times to repeat
+    distance = 100  # how many times to repeat
     step = 2  # count by 2
 
     # loop purpose is to animate ship sailing
     for position in range(start, distance, step):
-        ship_print(position)  # call to function with parameter
+        Animation_print(position)  # call to function with parameter
         time.sleep(.1)
+
+animation()
